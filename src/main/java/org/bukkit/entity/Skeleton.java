6db839392933fa316d9658a7cdb213ee30f40440
feildmaster <admin@feildmaster.com>
2012-11-03 19:54:24 -0500

    Add 1.4.2 skeleton API

--- src/main/java/org/bukkit/entity/Skeleton.java
+++ src/main/java/org/bukkit/entity/Skeleton.java
@@ -3,4 +3,16 @@ package org.bukkit.entity;
 /**
  * Represents a Skeleton.
  */
-public interface Skeleton extends Monster {}
+public interface Skeleton extends Monster {
+    public enum Type {
+        NORMAL, WITHER;
+    }
+
+    /**
+     * Get the skeleton type
+     * @return Type of skeleton
+     */
+    Type getSkeletonType();
+
+    void setSkeletonType(Type type);
+}
